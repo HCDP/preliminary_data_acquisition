6c0b42ea94d7d990d8d0aaa4d528e64b8c00358f
jardmclan <mcleanj@hawaii.edu>
2025-07-17 12:07:39 -1000

    fix mesonet synoptic task

--- containers/hi_mesonet_synoptic/task.sh
+++ containers/hi_mesonet_synoptic/task.sh
@@ -11,7 +11,7 @@ fi
 echo "Acquisition date is: " $CUSTOM_DATE
 
 echo "[task.sh] [2/4] Collecting Climate data from Synoptic on the daily timeframe."
-cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/hi_mesonet
+cd /home/hawaii_climate_products_container/preliminary/data_aqs/code/hi_mesonet_synoptic
 echo "---hiMeso_synoptic_24hr_data_aqs.R---"
 Rscript hiMeso_synoptic_24hr_data_aqs.R $CUSTOM_DATE
 
